30a394c26e478aa5852b347b3f6c29d1183cb0c0
Lili Enseling <liliyang@ad.unc.edu>
2023-10-18 23:30:17 -0400

    Player movement animation!

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,26 +1,48 @@
 extends CharacterBody2D
 
-var speed: int = 300
-var jump_speed: int = 250
-var gravity: int = 400
+@onready var anim = $AnimatedSprite2D
+
+@export var SPEED: int = 150
+@export var JUMP_FORCE: int = 250
+@export var GRAVITY: int = 900
+var is_moving: bool = false
+
+func _ready():
+	anim.play("Idle")  # Start with the "Idle" animation
 
 func get_input(delta):
 	velocity.x = 0
-	if Input.is_action_pressed("move_right"):
-		velocity.x += speed
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= speed
+
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+	if input.x != 0:
+		velocity.x = input.x * SPEED
+		if !is_moving:
+			is_moving = true
+			anim.play("Run")
+	else:
+		if is_moving:
+			is_moving = false
+			anim.play("Idle")
+
 	if Input.is_action_pressed("jump"):
 		if (is_on_floor()):
-			velocity.y -= jump_speed
-	
+			velocity.y -= JUMP_FORCE
+
 	# gravity
-	velocity.y += gravity * delta
+	velocity.y += GRAVITY * delta
 	set_velocity(velocity)
 	set_up_direction(Vector2.UP)
 	move_and_slide()
-	velocity = velocity
-	pass
 
 func _physics_process(delta):
 	get_input(delta)
+	
+	var direction = Input.get_axis("move_left", "move_right")
+
+	# Rotate
+	if direction == 1:
+		anim.flip_h = false
+	elif direction == -1:
+		anim.flip_h = true
