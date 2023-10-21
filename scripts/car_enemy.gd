e475f17c39dcdc35d1e82648f6ec86da141b4545
Lili Enseling <liliyang@ad.unc.edu>
2023-10-20 23:52:15 -0400

    Add moving car enemy
    Colliding with car will reset the scene.

---
+++ scripts/car_enemy.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+var speed = -30.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var facing_right = false
+
+func _ready():
+	$AnimatedSprite2D.play("Moving")
+	
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	
+	velocity.x = speed
+	move_and_slide()
+
+func flip():
+	facing_right = !facing_right
+	
+	scale.x = abs(scale.x) * -1
+	speed = -speed
