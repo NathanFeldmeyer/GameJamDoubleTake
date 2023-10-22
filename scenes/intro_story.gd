15ee45ab2654225793b465431308a79cab58ab41
Alondra Alfaro Bonilla <107895705+aanahid@users.noreply.github.com>
2023-10-21 23:04:06 -0400

    Intro story :D

---
+++ scenes/intro_story.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_continue_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/intro_story.tscn")
 
 func _on_guide_pressed():
 	pass # Replace with function body.
