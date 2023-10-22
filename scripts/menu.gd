17ab898346c2f61a080c826d43e448d3fc3bc04f
Alondra Alfaro Bonilla <107895705+aanahid@users.noreply.github.com>
2023-10-22 09:55:01 -0400

    Level 1 with tips and guide button works

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+@onready var guide = $PopUp
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/intro_story.tscn")
 
 func _on_guide_pressed():
-	pass # Replace with function body.
+	guide.show()
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+func _on_x_pressed():
+	guide.hide()
